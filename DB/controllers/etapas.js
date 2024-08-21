7d538e854a30ebd743e93b81ff07f157bd1c0cc7
Eric Gerzenstein <112182259+elmUffins@users.noreply.github.com>
2024-08-21 10:42:10 -0300

    fix: cambiar funciones create y update, arreglar nombres

--- DB/controllers/etapas.js
+++ DB/controllers/etapas.js
@@ -2,31 +2,31 @@ import {client} from "../db.js";
 
 const getEtapas = async (_, res) => {
 
-    const rows = await client.query('SELECT * FROM ejercicios');
+    const rows = await client.query('SELECT * FROM etapas');
     res.json(rows);
     }
 
 const getEtapa = async (req, res) => {
 
     const id = req.params.id
-    const rows = await client.query("SELECT * FROM ejercicios WHERE id = $1", [id])
+    const rows = await client.query("SELECT * FROM etapas WHERE id = $1", [id])
     res.json(rows[0])
 }
 
 const createEtapa = async (req, res) => {
 
-    const user = req.body
-    const email = req.body
-    const contraseña = req.body
-    await client.query("INSERT INTO ejercicios (user, email, contraseña ) VALUES ($1)", [user], [email], [contraseña])
-    res.json(user)
+    const {nombre, descripcion, id_ejercicio} = req.body
+
+    await client.query("INSERT INTO etapas (nombre, descripcion, id_ejercicio) VALUES ($1)",
+    [nombre], [descripcion], [id_ejercicio])
+    res.json({nombre, descripcion, id_ejercicio})
 };
 
 const updateEtapa = async (req, res) => {
 
     const nombre = req.body
     const id = req.params.id
-    await client.query("UPDATE ejercicios SET nombre = ? WHERE id = $1", [nombre, id])
+    await client.query("UPDATE etapas SET nombre = $1 WHERE id = $2", [nombre, id])
 
     res.json(nombre)
 };
@@ -34,7 +34,7 @@ const updateEtapa = async (req, res) => {
 const deleteEtapa = async (req, res) => {
 
     const id = req.params.id
-    await client.query("DELETE FROM ejercicios WHERE id = $1", [id])
+    await client.query("DELETE FROM etapas WHERE id = $1", [id])
     res.json(id)
 };
 
