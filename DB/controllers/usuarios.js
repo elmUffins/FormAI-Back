9ec63eaa3dbf4959f880d1d84d79de6f48dbfe46
Eric Gerzenstein <112182259+elmUffins@users.noreply.github.com>
2024-08-29 11:49:22 -0300

    fix: pedir solo las rows, borrar espacios

--- DB/controllers/usuarios.js
+++ DB/controllers/usuarios.js
@@ -1,35 +1,30 @@
-import {client} from "../db.js";
+import { client } from "../db.js";
 
 const getUsuarios = async (_, res) => {
-
-    const rows = await client.query('SELECT * FROM usuarios')
+    const { rows } = await client.query('SELECT * FROM usuarios')
     res.json(rows)
 };
 
 const getUsuario = async (req, res) => {
-
     const id = req.params.id
-    const rows = await client.query("SELECT * FROM usuarios WHERE id = $1", [id])
+    const { rows } = await client.query("SELECT * FROM usuarios WHERE id = $1", [id])
     res.json(rows[0])
 };
 
 const createUsuario = async (req, res) => {
-
-    const {usuario, email, pass} = req.body
+    const { usuario, email, pass } = req.body
     await client.query("INSERT INTO usuarios (usuario, email, pass) VALUES ($1, $2, $3)", [usuario, email, pass])
-    res.json({usuario, email, pass})
+    res.json({ usuario, email, pass })
 };
 
 const updateUsuario = async (req, res) => {
-
-    const {usuario} = req.body
+    const { usuario } = req.body
     const id = req.params.id
     await client.query("UPDATE usuarios SET usuario = $1 WHERE id = $2", [usuario, id])
-    res.json({usuario, id})
+    res.json({ usuario, id })
 };
 
 const deleteUsuario = async (req, res) => {
-
     const id = req.params.id
     await client.query("DELETE FROM usuarios WHERE id = $1", [id])
     res.json(id)
