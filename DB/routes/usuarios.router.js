200dd558068f9c8cad9dbb35c25f6607a7f4f95f
Eric Gerzenstein <112182259+elmUffins@users.noreply.github.com>
2024-09-23 14:26:17 -0300

    feat: add use of auth in routes

--- DB/routes/usuarios.router.js
+++ DB/routes/usuarios.router.js
@@ -1,15 +1,17 @@
 import Router from 'express';
 import usuariosController from '../controllers/usuarios.controllers.js';
+import auth from '../middleware/auth.js';
 
 const router = Router();
 
 // Define routes
 router.post('/register', usuariosController.register);
 router.post('/login', usuariosController.logIn);
-router.get('/', usuariosController.getUsuarios);
-router.get('/:id', usuariosController.getUsuario);
-router.post('/', usuariosController.createUsuario);
-router.put('/:id', usuariosController.updateUsuario);
-router.delete('/:id', usuariosController.deleteUsuario);
+router.get('/', auth.verifyToken, auth.verifyAdmin, usuariosController.getUsuarios);
+router.get('/:id', auth.verifyToken, auth.verifyAdmin, usuariosController.getUsuario);
+router.post('/', auth.verifyToken, auth.verifyAdmin, usuariosController.createUsuario);
+router.put('/:id', auth.verifyToken, usuariosController.updateUsuario);
+router.put('/:id/promote', auth.verifyToken, auth.verifyAdmin, )
+router.delete('/:id', auth.verifyToken, auth.verifyAdmin, usuariosController.deleteUsuario);
 
 export default router;
