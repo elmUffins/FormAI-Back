1e074563754963513f2739efeff148409924cc8c
Eric Gerzenstein <112182259+elmUffins@users.noreply.github.com>
2024-09-24 11:43:19 -0300

    feat: create logic for video table

---
+++ DB/routes/videos.router.js
@@ -0,0 +1,16 @@
+import Router from 'express';
+import usuariosController from '../controllers/usuarios.controllers.js';
+import auth from '../middleware/auth.js';
+
+const router = Router();
+
+// Define routes
+router.get('/', auth.verifyToken, auth.verifyAdmin, );
+router.get('/:id', auth.verifyToken, auth.verifyAdmin, );
+router.post('/', auth.verifyToken, auth.verifyAdmin, );
+router.put('/:id', auth.verifyToken, );
+router.put('/:id/promote', auth.verifyToken, auth.verifyAdmin, )
+router.delete('/:id', auth.verifyToken, auth.verifyAdmin, );
+
+export default router;
+
