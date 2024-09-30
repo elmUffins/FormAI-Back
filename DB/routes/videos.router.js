a587133cc815190510167b443531bcd5fe7c495e
Eric Gerzenstein <112182259+elmUffins@users.noreply.github.com>
2024-09-30 10:05:59 -0300

    feat: add video routes

--- DB/routes/videos.router.js
+++ DB/routes/videos.router.js
@@ -5,12 +5,12 @@ import auth from '../middleware/auth.js';
 const router = Router();
 
 // Define routes
-router.get('/', auth.verifyToken, auth.verifyAdmin, );
-router.get('/:id', auth.verifyToken, auth.verifyAdmin, );
-router.post('/', auth.verifyToken, auth.verifyAdmin, );
-router.put('/:id', auth.verifyToken, );
-router.put('/:id/promote', auth.verifyToken, auth.verifyAdmin, )
-router.delete('/:id', auth.verifyToken, auth.verifyAdmin, );
+router.get('/', auth.verifyToken, auth.verifyAdmin, videosController.getVideos);
+router.get('/:id', auth.verifyToken, videosController.getVideo);
+router.get('/usuario', auth.verifyToken, videosController.getVideosByUsuario);
+router.post('/', auth.verifyToken, videosController.uploadVideo);
+router.put('/:id', auth.verifyToken, videosController.deleteVideo);
+
 
 export default router;
 
