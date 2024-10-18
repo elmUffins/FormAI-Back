38a0dc807a7e11019ed844d9a6015e33e889ae54
elmUffins <ericyes81007@gmail.com>
2024-10-17 21:52:23 -0300

    style: rename

--- DB/index.js
+++ DB/index.js
@@ -3,19 +3,14 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-import ejerciciosRouter from './routers/ejercicios.router.js';
-import usuariosRouter from './routers/usuarios.router.js';
-import videosRouter from './routers/videos.router.js';
+import ejerciciosRouter from './routes/ejercicios.router.js';
+import usuariosRouter from './routes/usuarios.router.js';
+import videosRouter from './routes/videos.router.js';
 
 import "dotenv/config";
 
 app.use(express.json());
-app.use(cors());
 
-var corsOptions = {
-    origin: 'http://localhost/3000',
-    optionsSuccessStatus: 200
-  }
 
 app.get("/", (_, res) => {
     res.send("1");
