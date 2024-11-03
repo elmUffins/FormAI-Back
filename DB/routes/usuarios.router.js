6f27473afc090633136d86f0af983e31bac4b3c1
Eric <112182259+elmUffins@users.noreply.github.com>
2024-11-03 19:46:22 -0300

    fix: tra

--- DB/routes/usuarios.router.js
+++ DB/routes/usuarios.router.js
@@ -8,7 +8,7 @@ const router = Router();
 router.post('/register', usuariosController.register);
 router.post('/login', usuariosController.login);
 router.get('/', auth.verifyToken, auth.verifyAdmin, usuariosController.getUsuarios);
-router.get('/:id', auth.verifyToken, auth.verifyAdmin, usuariosController.getUsuario);
+router.get('/:id', auth.verifyToken, usuariosController.getUsuario);
 router.post('/', auth.verifyToken, auth.verifyAdmin, usuariosController.createUsuario);
 router.put('/:id', auth.verifyToken, usuariosController.updateUsuario);
 router.put('/promote/:id', auth.verifyToken, auth.verifyAdmin, usuariosController.promoteUsuario);
